0d30f44fa4350747937d45f13c710fa39c655c8c
Andrew Kornilov <frutik@gmail.com>
2012-02-07 20:56:15 +0200

    -

--- handlers/GetLatestEventHandler.py
+++ handlers/GetLatestEventHandler.py
@@ -19,9 +19,23 @@ class GetLatestEventHandler(tornado.web.RequestHandler):
     schedule_time = 0.2
     current_handle = None
 
-    @tornado.web.asynchronous
+#    @tornado.web.asynchronous
     def get(self):
-        self.schedule_execution(self.schedule_time, self.loop)
+#        self.schedule_execution(self.schedule_time, self.loop)
+        try:
+            events = QueueLog.select().orderBy('-id').limit(1)
+            event_id = events[0].id
+
+        except:
+            event_id = 0
+            return
+
+        self.set_header("Content-Type", 'text/javascript')
+        self.write("var last_event = '" + str(event_id) + "';")
+
+#        self.finish()
+
+
 
     def schedule_execution(self, schedule_time, callback):
         self.handle = tornado.ioloop.IOLoop.instance().add_timeout(time.time() + schedule_time, callback)
