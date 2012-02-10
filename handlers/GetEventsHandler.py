1ea783884f365d0a45a97b218bc82ed45e224d6f
Andrew Kornilov <frutik@gmail.com>
2012-02-10 23:21:31 +0200

    -

--- handlers/GetEventsHandler.py
+++ handlers/GetEventsHandler.py
@@ -7,11 +7,12 @@ import logging
 class GetEventsHandler(tornado.web.RequestHandler):
 
     schedule_time = 0.2
-    current_handle = None
+    delimiter = self.delimiter
 
     def initialize(self, **kwargs):
         super(GetEventsHandler, self).initialize()
         self.QueueLog = kwargs['QueueLog']
+        self.current_handle = None
 
     @tornado.web.asynchronous
     def get(self, last_event_id):
@@ -61,15 +62,114 @@ class GetEventsHandler(tornado.web.RequestHandler):
 
         self.finish()
 
-    def _completeagent_data(self, data):
-        return {}
+    def _parsed_data(self, keys, data):
+        t = data.split(self.delimiter)
+        r = {}
+        for i,n in enumerate(keys):
+            r[n] = t[i]
 
-    def _completecaller_data(self, data):
-        return {}
+        return r
 
     def _abandon_data(self, data):
+        return self._parsed_data(['position', 'origposition', 'waittime'], data)
+#        t = data.split(self.delimiter)
+#        return {
+#            'position': t[0],
+#            'origposition': t[1],
+#            'waittime': t[2],
+#        }
+
+    def _agentlogin_data(self, data):
+        t = data.split(self.delimiter)
         return {
-            'test':'test'
+            'channel': t[0],
+        }
+
+    def _agentcallbacklogin_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'exten@context': t[0],
+        }
+
+    def _agentlogoff_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'channel': t[0],
+            'logintime': t[1],
+        }
+
+    def _agentcallbacklogoff_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'exten@context': t[0],
+            'logintime': t[1],
+            'reason': t[2],
+        }
+
+    def _completeagent_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'holdtime': t[0],
+            'calltime': t[1],
+            'origposition': t[2],
+        }
+
+    def _completecaller_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'holdtime': t[0],
+            'calltime': t[1],
+            'origposition': t[2],
+        }
+
+    def _connect_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'holdtime': t[0],
+            'bridgedchanneluniqueid': t[1],
+        }
+
+    def _enterqueue_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'url': t[0],
+            'callerid': t[1],
+        }
+
+    def _exitempty_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'position': t[0],
+            'origposition': t[1],
+            'waittime': t[2],
+        }
+
+    def _exitwithkey_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'key': t[0],
+            'position': t[1],
+        }
+
+    def _exitwithtimeout_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'position': t[0],
+        }
+
+    def _ringnoanswer_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'ringtime': t[0],
+        }
+
+    def _transfer_data(self, data):
+        t = data.split(self.delimiter)
+        return {
+            'extension': t[0],
+            'context': t[1],
+            'holdtime': t[2],
+            'calltime': t[3],
         }
 
     def on_connection_close(self):
